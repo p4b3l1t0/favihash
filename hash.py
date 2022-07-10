a8e10d45510f7d94b73b8482e57cea0da1046a44
P4b3l1t0 <59421032+p4b3l1t0@users.noreply.github.com>
2022-07-10 14:35:13 -0300

    Delete hash.py

--- hash.py
+++
@@ -1,8 +0,0 @@
-import mmh3
-import requests
-import codecs
- 
-response = requests.get('https://www.example.com.ar/favicon.ico')
-favicon = codecs.encode(response.content,"base64")
-hash = mmh3.hash(favicon)
-print(hash)
